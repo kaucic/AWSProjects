921d9b56d59d5330326e9a86f761f6c5488e6875
unknown <rakaucic2@gmail.com>
2021-09-07 15:43:34 -0700

    fixed Farkle scoring and wrote test cases

--- Farkle/DiceRollGame/Farkle.py
+++ Farkle/DiceRollGame/Farkle.py
@@ -13,11 +13,12 @@ class Farkle:
     def __init__(self):
         return
 
-    # Method to compute the Farkle score pertaining to the binary vector diceToScore
+    # Method to compute the Farkle score pertaining to the binary vector diceToScore with spots in diceVals
     # Return the score and the dice that scored
     # TODO: complete and fix the scoring and return the dice that scored instead of the number of dice used     
     def scoreDice(self, diceVals, diceToScore)  -> Tuple[int,list]:
         count = 0
+        dice_that_scored = 0
         vals = {}
         # Populate hash table
         for i in range(len(diceVals)):
@@ -28,28 +29,53 @@ class Farkle:
                 else:
                     vals[diceVals[i]] = 1
         
-        score = 0
-        if count > 2:
-            for key, val in vals.items():
-                if val == 6:
-                    score = 3000
-                if val == 5:
-                    score = 2000
-                elif val == 4:
-                    score = 1000
-                elif val == 3:
-                    score = 100 * key
-                    if key == 1:
-                        score = 300
-
-        if 1 in vals:
-            if vals[1] < 3:
-                score += vals[1] * 100
-        if 5 in vals:
-            if vals[5] < 3:
-                score += vals[5] * 50
         
-        return score, count
+        # Check for a straight
+        num_pairs = 0
+        num_triplets = 0
+        if 1 in vals and 2 in vals and 3 in vals and 4 in vals and 5 in vals and 6 in vals:
+            score = 1500
+            dice_that_scored = 6
+        else:
+            score = 0
+            if count > 2:
+                for key, val in vals.items():
+                    if val == 6:
+                        score = 3000
+                        dice_that_scored = 6
+                    if val == 5:
+                        score = 2000
+                        dice_that_scored = 5
+                    elif val == 4:
+                        score = 1000
+                        dice_that_scored = 4
+                    elif val == 3:
+                        num_triplets += 1
+                        score = 100 * key
+                        if key == 1:
+                            score = 300
+                        dice_that_scored += 3
+                    elif val == 2:
+                        num_pairs += 1
+
+            if 1 in vals:
+                if vals[1] < 3:
+                    score += vals[1] * 100
+                    dice_that_scored += vals[1]
+            if 5 in vals:
+                if vals[5] < 3:
+                    score += vals[5] * 50
+                    dice_that_scored += vals[5]
+
+        # Fix score for Three pairs and Two triplets
+        if num_triplets == 2:
+            score = 2500
+            dice_that_scored = 6
+        elif num_pairs == 3:
+            score = 1500
+            dice_that_scored = 6
+        
+        return score, dice_that_scored
 
 
 # Start main program
@@ -59,5 +85,5 @@ if __name__ == "__main__":
     dice = [6, 5, 1, 5, 5, 5]
     keptDice = [True, True, False, True, True, True]
     score, scoringDice = inst.scoreDice(dice,keptDice)
-    logging.info(f"dice are {dice} score is {score} count is {scoringDice}")
+    logging.info(f"dice are {dice} keptDice are {keptDice} score is {score} count is {scoringDice}")
     doFlaskLogging.clean_up_logger()
---
+++ Farkle/DiceRollGame/testFarkle.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 07 13:50:26 2021
+
+@author: home
+"""
+
+import unittest
+import warnings
+import sys
+
+import logging
+from Farkle import Farkle
+
+class TestFarkle(unittest.TestCase):
+
+    def test(self):
+        warnings.simplefilter("ignore", ResourceWarning)
+        try:
+            inst = Farkle()
+            dice = [3, 5, 3, 3, 6, 1]
+            keptDice = [True, True, False, True, True, True]
+            score, scoringDice = inst.scoreDice(dice,keptDice)
+            logging.info(f"dice are {dice} keptDice are {keptDice} score is {score} num scoringDice is {scoringDice}")
+            if score != 150 or scoringDice != 2:
+                 self.fail(f"test failed. score is {score} scoringDice is {scoringDice}")            
+        except Exception as err:
+            print("Error Message {0}".format(err))
+            sys.exit(1)
+            
+    def test_straight(self):
+        try:
+            inst = Farkle()
+            dice = [6, 5, 3, 4, 1, 2]
+            keptDice = [True, True, True, True, True, True]
+            score, scoringDice = inst.scoreDice(dice,keptDice)
+            logging.info(f"dice are {dice} keptDice are {keptDice} score is {score} num scoringDice is {scoringDice}")
+            if score != 1500 or scoringDice != 6:
+                 self.fail(f"test_straight failed. score is {score} scoringDice is {scoringDice}")            
+        except Exception as err:
+            print("Error Message {0}".format(err))
+            sys.exit(1)
+
+    def test_Farkle(self):
+        try:
+            inst = Farkle()
+            dice = [2, 1, 4, 5, 6, 6]
+            keptDice = [True, False, True, False, True, True]
+            score, scoringDice = inst.scoreDice(dice,keptDice)
+            logging.info(f"dice are {dice} keptDice are {keptDice} score is {score} num scoringDice is {scoringDice}")
+            if score != 0 or scoringDice != 0:
+                 self.fail(f"test_Farkle failed. score is {score} scoringDice is {scoringDice}")            
+        except Exception as err:
+            print("Error Message {0}".format(err))
+            sys.exit(1)
+
+    def test_three_pairs(self):
+        try:
+            inst = Farkle()
+            dice = [2, 1, 4, 4, 2, 1]
+            keptDice = [True, True, True, True, True, True]
+            score, scoringDice = inst.scoreDice(dice,keptDice)
+            logging.info(f"dice are {dice} keptDice are {keptDice} score is {score} num scoringDice is {scoringDice}")
+            if score != 1500 or scoringDice != 6:
+                 self.fail(f"test_Farkle failed. score is {score} scoringDice is {scoringDice}")            
+        except Exception as err:
+            print("Error Message {0}".format(err))
+            sys.exit(1)
+
+    def test_two_triplets(self):
+        try:
+            inst = Farkle()
+            dice = [6, 5, 6, 6, 5, 5]
+            keptDice = [True, True, True, True, True, True]
+            score, scoringDice = inst.scoreDice(dice,keptDice)
+            logging.info(f"dice are {dice} keptDice are {keptDice} score is {score} num scoringDice is {scoringDice}")
+            if score != 2500 or scoringDice != 6:
+                 self.fail(f"test_Farkle failed. score is {score} scoringDice is {scoringDice}")            
+        except Exception as err:
+            print("Error Message {0}".format(err))
+            sys.exit(1)
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    unittest.main()
